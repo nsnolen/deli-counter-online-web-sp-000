ddb4bfca68606d16c83a471e3b1adce5c2719c67
Sara Nolen <nolensara1@gmail.com>
2020-11-30 16:49:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,6 +11,7 @@ def take_a_number(katz_deli, name)
   #array.push("#{name}")
   #array.push("Ada")
   #katz_deli.each_with_index{|name, index| array << "Welcome #{name}, you are number #{index + 1} in line."}
+  array = []
   array.push("#{name}")
 end
 
