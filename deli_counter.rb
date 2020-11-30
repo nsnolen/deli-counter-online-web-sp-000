7b81676dfab27ece52f394d31b8d5181324eb010
Sara Nolen <nolensara1@gmail.com>
2020-11-30 14:46:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 def line(katz_deli)
+#iterating through the katz_deli array and returning different value
   array = []
   katz_deli.each_with_index{|name, index| array = "#{name} #{index}"}
+  array
   if katz_deli.length == 0
     "The line is currently empty."
   else
