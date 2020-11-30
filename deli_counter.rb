485385466c2fe3c93002885365698662ecc857c5
Sara Nolen <nolensara1@gmail.com>
2020-11-30 15:46:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ end
 def take_a_number(katz_deli, name)
   array = []
   katz_deli.each_with_index{|name, index| array << "#{index + 1}"}
-  array.push("#{name}") if array.length == 0
+  array.unshift("#{name}") if array.length == 0
   puts "Welcome, #{name}. You are number #{array.join(' ')} in line." if array.length == 0
 end
 
