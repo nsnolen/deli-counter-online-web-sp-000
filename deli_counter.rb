dcf2d9421c7fc3de1be00cb7c72e1b773297b556
Sara Nolen <nolensara1@gmail.com>
2020-11-30 16:02:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def take_a_number(katz_deli, name)
   array = []
   katz_deli.each_with_index{|name, index| array << "Welcome #{name}, you are number #{index + 1} in line."}
   #array.push("#{name}")
-  puts "array.join" if array.length >= 0
+  array.join if array.length >= 0
 end
 
 
